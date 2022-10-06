f7671f97b6eaf91410ecf92f9bdb3b3a45db83f3
Yogesh Giri <88799492+madm234@users.noreply.github.com>
2022-10-06 22:48:25 +0530

    Add files via upload

---
+++ generator/func.js
@@ -0,0 +1,7 @@
+function wise()
+{
+    var as =document.querySelector(".as").value;
+    // var loc ="https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl="+as;
+    window.location.href ="https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl="+as;
+    console.log('as');
+}
